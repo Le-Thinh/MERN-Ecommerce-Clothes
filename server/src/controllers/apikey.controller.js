d43bf15cc74a598ac14e3f362b0e4e6b88a74b07
Le-Thinh <lqt9112003@gmail.com>
2025-12-17 18:02:22 +0700

    update

--- server/src/controllers/apikey.controller.js
+++ server/src/controllers/apikey.controller.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const { SuccessResponse } = require("../core/success.response.js");
-const apiKeyService = require("../services/apiKey.service.js");
+const apiKeyService = require("../services/apikey.service.js");
 
 class ApiKeyController {
   createApiKey = async (req, res, next) => {
